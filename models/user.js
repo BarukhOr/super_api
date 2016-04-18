20742ea7372dfb82b576464b7cfadbfa8a12655b
UtibeAbasi <1.BarukhOr@gmail.com>
2016-04-18 13:25:55 -0400

    Setup User Model

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const Mongoose = require('mongoose')
+const Schema = Mongoose.Schema
+
+//User Schema
+const userSchema = new Schema({
+	name:{
+		type:String
+	},
+	imgUrl:{
+		type:String
+	},
+	username:{
+		type:String,
+		unique:true,
+		lowercase:true
+	},
+	job:{
+		type:[Number]
+	},
+	password:{
+		type:String
+	}
+},{collection:'user'})
+
+
+userSchema.methods.comparePassword = function(candidatePassword,callback){
+	if(candidatePassword === this.password){
+		callback(null,true)
+	}else{
+		callback(null,false)
+	}
+}
+
+//Create the model class
+const ModelClass = Mongoose.model('staff',userSchema)
+
+//Export the model
+module.exports = ModelClass
