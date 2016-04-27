ac55224bf77905de4d25a052e43fa1db8fd8db63
UtibeAbasi <1.BarukhOr@gmail.com>
2016-04-27 13:53:10 -0400

    Active Directory
    + Updated Passport to utilize active directory for initial authentication

--- services/passport.js
+++ services/passport.js
@@ -1,24 +1,43 @@
-const passport = require('passport')
-const JwtStrategy = require('passport-jwt').Strategy
-const ExtractJwt = require('passport-jwt').ExtractJwt
-const LocalStrategy = require('passport-local')
-const User = require('../models/user')
-const config = require('../config')
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+const ActiveDirectory = require('activedirectory');
+
+const User = require('../models/user');
+const config = require('../config');
+
+//Store Mission Critical Secrets in this file
+const Config = require('../config');
 
 //Create local Strategy
 const localLogin = new LocalStrategy(function(username,password,done){
-	//Verify that the email/password pair is correct, otherwise return false to done
-	User.findOne({username:username},function(err,user){
-		if(err){return done(err)}
-		if(!user){return done(null,false)}
+	const localConfig = {
+		baseDN:Config.baseDN,
+		url:Config.ldap_url,
+		username:username+Config.domain,
+		password:password
+	}
 
-		//Compare password
-		user.comparePassword(password,function(err,isMatch){
-			if(err){return done(err)}
-			if(!isMatch){return done(null,false)}
+	const ad = new ActiveDirectory(localConfig)
 
-			return done(null,user)
-		})
+	ad.authenticate(localConfig.username,localConfig.password,function(err,auth){
+		if (err) {return done(err)}
+		if (auth){
+			User.findOne({username:username},function(err,user){
+				if (err) {return done(err)}
+				if (user){
+					return done(null,user)
+				}else{
+					console.log("user is a student, but is not a staff")
+					return done(null,false)
+				}
+			})
+		}else{
+			console.log("Authentication Failed")
+			return done(null,false)
+		}
+
 	})
 })
 
